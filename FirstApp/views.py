144c33fe10c0703dbb664edb99295360e97ab69f
Viswanthreddy <nivas0803@gmail.com>
2021-03-03 13:00:10 +0530

    update

--- FirstApp/views.py
+++ FirstApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from Printers import settings
+from django.contrib import messages
 # Create your views here.
 def home(req):
 	return render(req,'home.html')
+
+def contact(request):
+	if request.method=='POST':
+		name=request.POST['name']
+		email=request.POST['email']
+		subject=request.POST['subject']
+		message=request.POST['message']
+		sender=settings.EMAIL_HOST_USER
+		receiver=settings.EMAIL_HOST_USER
+		sub="Reg to your Customer Details..."
+		body="""Customer Name:{}\n Customer EmailID: {}\n Subject:{}\n Message:{}\n""".format(name,email,subject,message)
+		send_mail(sub,body,sender,[receiver])
+		#messages.success(request,'Data Submited Successfully')
+	return render(request,'home.html')
--- Printers/settings.py
+++ Printers/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +119,63 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='odinahelpdesk@gmail.com'
+EMAIL_HOST_PASSWORD='odina@2889'
